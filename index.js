b6c4131939e4339f500cb9b5ab450fb39913c85f
Allain Lalonde <allain.lalonde@gmail.com>
2021-03-03 17:02:04 -0500

    incorporating guard against prototype chain pollution from: https://github.com/418sec/propper/pull/1

--- index.js
+++ index.js
@@ -2,25 +2,25 @@ module.exports = prop;
 
 function prop(obj, path, value) {
   if (!obj) return undefined;
-  
+
   var isGet = arguments.length === 2;
 
   var parts = path;
-  if (typeof (path) === 'string') {
-    if (path.charAt(0) === '/') {
+  if (typeof path === "string") {
+    if (path.charAt(0) === "/") {
       // support /a/b/c
-      parts = path.substr(1).split('/');  
+      parts = path.substr(1).split("/");
     } else {
       //support a.b.c.
-      parts = path.split('.');
+      parts = path.split(".");
     }
   }
-  
-  var origParts = JSON.parse(JSON.stringify(parts));
-  parts = parts.reverse();  
 
-  var target = obj;  
-  
+  var origParts = JSON.parse(JSON.stringify(parts));
+  parts = parts.reverse();
+
+  var target = obj;
+
   var key = parts.pop();
   while (parts.length) {
     var nextTarget = target[key];
@@ -29,26 +29,29 @@ function prop(obj, path, value) {
         return undefined;
       } else {
         nextTarget = target[key] = {};
-      }      
+      }
     }
+
+    if (key == "__proto__" || key == "prototype" || key == "constructor")
+      return undefined;
 
     target = nextTarget;
     key = parts.pop();
-  }  
+  }
 
   if (isGet) {
     return target[key];
-  } 
+  }
 
   if (value !== undefined) {
     target[key] = value;
     return value;
   }
 
-  delete(target[key]);
+  delete target[key];
   if (Object.keys(target).length === 0 && origParts.length > 1) {
-      // Since the case of recursive delete is infrequent, this is likely performant enough.    
-      origParts.pop();
-      prop(obj, origParts, undefined);          
+    // Since the case of recursive delete is infrequent, this is likely performant enough.
+    origParts.pop();
+    prop(obj, origParts, undefined);
   }
 }
--- test.js
+++ test.js
@@ -1,112 +1,119 @@
-var test = require('tape');
-var prop = require('./index.js');
+var test = require("tape");
+var prop = require("./index.js");
 
-test('non-object', function(t) {
+test("non-object", function (t) {
   t.plan(3);
-  t.equal(prop(false, 'a'), undefined);
-  t.equal(prop(false, '/a'), undefined);
-  t.equal(prop(false, ['a']), undefined);
+  t.equal(prop(false, "a"), undefined);
+  t.equal(prop(false, "/a"), undefined);
+  t.equal(prop(false, ["a"]), undefined);
 });
 
-test('null object', function(t) {
+test("null object", function (t) {
   t.plan(3);
-  t.equal(prop(null, 'a'), undefined);
-  t.equal(prop(null, '/a'), undefined);
-  t.equal(prop(null, ['a']), undefined);
+  t.equal(prop(null, "a"), undefined);
+  t.equal(prop(null, "/a"), undefined);
+  t.equal(prop(null, ["a"]), undefined);
 });
 
-test('undefined object', function(t) {
+test("undefined object", function (t) {
   t.plan(3);
-  t.equal(prop(undefined, 'a'), undefined);
-  t.equal(prop(undefined, '/a'), undefined);
-  t.equal(prop(undefined, ['a']), undefined);
+  t.equal(prop(undefined, "a"), undefined);
+  t.equal(prop(undefined, "/a"), undefined);
+  t.equal(prop(undefined, ["a"]), undefined);
 });
 
-test('empty object', function (t) {  
-    t.plan(5);
+test("empty object", function (t) {
+  t.plan(5);
 
-    t.equal(prop({}, 'a.b'), undefined);
-    t.equal(prop({}, '/a/b'), undefined);
-    t.equal(prop({}, ['a', 'b']), undefined);
+  t.equal(prop({}, "a.b"), undefined);
+  t.equal(prop({}, "/a/b"), undefined);
+  t.equal(prop({}, ["a", "b"]), undefined);
 
-    t.equal(prop({}, ''), undefined);
-    t.equal(prop({}, []), undefined);
+  t.equal(prop({}, ""), undefined);
+  t.equal(prop({}, []), undefined);
 });
 
-test('set shallow value', function(t) {
+test("set shallow value", function (t) {
   t.plan(1);
 
   var obj = {};
-  
-  prop(obj, 'a', true);
-  prop(obj, 'b', 1);
-  prop(obj, 'c', 'hello');
+
+  prop(obj, "a", true);
+  prop(obj, "b", 1);
+  prop(obj, "c", "hello");
 
   t.deepEqual(obj, {
-    a: true, 
-    b: 1, 
-    c: 'hello'
+    a: true,
+    b: 1,
+    c: "hello",
   });
 });
 
-test('set deep value', function(t) {
+test("set deep value", function (t) {
   t.plan(1);
 
   var obj = {};
-  
-  prop(obj, 'a.b', true);
-  prop(obj, '/c/d', 1);
-  prop(obj, ['e', 'f'], 'hello');  
+
+  prop(obj, "a.b", true);
+  prop(obj, "/c/d", 1);
+  prop(obj, ["e", "f"], "hello");
 
   t.deepEqual(obj, {
-    a: {b: true}, 
-    c: {d: 1}, 
-    e: {f: 'hello'}    
+    a: { b: true },
+    c: { d: 1 },
+    e: { f: "hello" },
   });
 });
 
-test('seting deep value to undefined trims empty objects', function(t) {
+test("seting deep value to undefined trims empty objects", function (t) {
   t.plan(2);
 
-  var obj = {a: {b: {c: true}}};
-  prop(obj, 'a.b.c', undefined);
+  var obj = { a: { b: { c: true } } };
+  prop(obj, "a.b.c", undefined);
   t.deepEqual(obj, {});
 
-  var obj2 = {a: {b: {c: true}}, d: 1};
-  prop(obj2, 'a.b.c', undefined);
-  t.deepEqual(obj2, {d: 1});
+  var obj2 = { a: { b: { c: true } }, d: 1 };
+  prop(obj2, "a.b.c", undefined);
+  t.deepEqual(obj2, { d: 1 });
 });
 
-test('get prop on null returns undefined', function(t) {
+test("get prop on null returns undefined", function (t) {
   t.plan(1);
 
-  t.equal(prop(null, 'a'), undefined);
+  t.equal(prop(null, "a"), undefined);
 });
 
-test('get shallow value', function(t) {
+test("get shallow value", function (t) {
   t.plan(4);
 
-  var obj = {a: true, b: 1, c: 'hello'};
-  
-  t.equal(prop(obj, 'a'), true);
-  t.equal(prop(obj, '/b'), 1);
-  t.equal(prop(obj, ['c']), 'hello');
-  t.equal(prop(obj, 'd'), undefined); 
+  var obj = { a: true, b: 1, c: "hello" };
+
+  t.equal(prop(obj, "a"), true);
+  t.equal(prop(obj, "/b"), 1);
+  t.equal(prop(obj, ["c"]), "hello");
+  t.equal(prop(obj, "d"), undefined);
 });
 
-test('get deep value', function(t) {
+test("get deep value", function (t) {
   t.plan(4);
 
-  var obj = {a: {b: true}, c: {d: 1}, e: {f: 'hello'}};
-  
-  t.equal(prop(obj, 'a.b'), true);
-  t.equal(prop(obj, '/c/d'), 1);
-  t.equal(prop(obj, ['e', 'f']), 'hello');  
-  t.equal(prop(obj, 'g.h'), undefined);
+  var obj = { a: { b: true }, c: { d: 1 }, e: { f: "hello" } };
+
+  t.equal(prop(obj, "a.b"), true);
+  t.equal(prop(obj, "/c/d"), 1);
+  t.equal(prop(obj, ["e", "f"]), "hello");
+  t.equal(prop(obj, "g.h"), undefined);
 });
 
-test('returns undefined when non object is passed as src', function(t) {
-  t.equal(prop(null, 'a'), undefined)
-  t.equal(prop(undefined, 'a'), undefined)
-  t.end()
+test("returns undefined when non object is passed as src", function (t) {
+  t.equal(prop(null, "a"), undefined);
+  t.equal(prop(undefined, "a"), undefined);
+  t.end();
+});
+
+test("is not susceptible to prototype chain pollution", function (t) {
+  var obj = {};
+  prop(obj, "__proto__.polluted", "polluted");
+  t.equal(obj.polluted, undefined);
+  t.end();
 });
